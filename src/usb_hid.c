a33e82be819c50b0a17885abca8034ac2f7ddfa3
math-milan <lovis.ramthun@icatas.eu>
2025-02-27 13:52:01 +0100

    fixed formatting

--- src/usb_hid.c
+++ src/usb_hid.c
@@ -16,8 +16,7 @@
 //--------------------------------------------------------------------+
 // Device Descriptors
 //--------------------------------------------------------------------+
-tusb_desc_device_t const desc_device =
-{
+tusb_desc_device_t const desc_device = {
     .bLength            = sizeof(tusb_desc_device_t),
     .bDescriptorType    = TUSB_DESC_DEVICE,
     .bcdUSB             = USB_BCD,
@@ -39,22 +38,19 @@ tusb_desc_device_t const desc_device =
 
 // Invoked when received GET DEVICE DESCRIPTOR
 // Application return pointer to descriptor
-uint8_t const * tud_descriptor_device_cb(void)
-{
+uint8_t const * tud_descriptor_device_cb(void){
   return (uint8_t const *) &desc_device;
 }
 
 // Invoked when received GET HID REPORT DESCRIPTOR
-uint8_t const * tud_hid_descriptor_report_cb(uint8_t instance)
-{
+uint8_t const * tud_hid_descriptor_report_cb(uint8_t instance){
     usb_manager_t *manager = get_usb_manager();
   
     return manager->usb_hid_desc;
     // return desc_hid_report_1;
 }
 
-enum
-{
+enum {
   ITF_NUM_HID,
   ITF_NUM_TOTAL
 };
@@ -63,20 +59,18 @@ enum
 
 #define EPNUM_HID   0x81
 
-uint8_t desc_configuration[] =
-{
-  // Config number, interface count, string index, total length, attribute, power in mA
-  TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 500),
+uint8_t desc_configuration[] = {
+    // Config number, interface count, string index, total length, attribute, power in mA
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 500),
 
-  // Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
-  TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, 0, EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 5) // the length her 0 is at 25 index
+    // Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
+    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, 0, EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 5) // the length her 0 is at 25 index
 };
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
 // Descriptor contents must exist long enough for transfer to complete
-uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
-{
+uint8_t const * tud_descriptor_configuration_cb(uint8_t index){
     usb_manager_t *manager = get_usb_manager();
 
     desc_configuration[25] = manager->usb_hid_desc_length;
@@ -88,30 +82,26 @@ uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 //--------------------------------------------------------------------+
 
 // array of pointer to string descriptors
-char const* string_desc_arr [] =
-{
-  (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "KNOB",                        // 1: Manufacturer
-  "RP2040",                      // 2: Product
-  "123456",                      // 3: Serials, should use chip ID
+char const* string_desc_arr [] = {
+    (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
+    "KNOB",                        // 1: Manufacturer
+    "RP2040",                      // 2: Product
+    "123456",                      // 3: Serials, should use chip ID
 };
 
 static uint16_t _desc_str[32];
 
 // Invoked when received GET STRING DESCRIPTOR request
 // Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
-uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
-{
-  (void) langid;
+uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid){
+    (void) langid;
 
-  uint8_t chr_count;
+    uint8_t chr_count;
 
-  if ( index == 0)
-  {
-    memcpy(&_desc_str[1], string_desc_arr[0], 2);
-    chr_count = 1;
-  }else
-  {
+    if ( index == 0){
+        memcpy(&_desc_str[1], string_desc_arr[0], 2);
+        chr_count = 1;
+    }else{
     // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
     // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
 
@@ -122,31 +112,28 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
     // Cap at max char
     chr_count = strlen(str);
     if ( chr_count > 31 ) chr_count = 31;
-
-    // Convert ASCII string into UTF-16
-    for(uint8_t i=0; i<chr_count; i++)
-    {
-      _desc_str[1+i] = str[i];
+        // Convert ASCII string into UTF-16
+        for(uint8_t i=0; i<chr_count; i++)
+        {
+            _desc_str[1+i] = str[i];
+        }
     }
-  }
 
-  // first byte is length (including header), second byte is string type
-  _desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
+    // first byte is length (including header), second byte is string type
+    _desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
 
-  return _desc_str;
+    return _desc_str;
 }
 
 // Callbacks
 // Invoked when device is mounted
-void tud_mount_cb(void)
-{
+void tud_mount_cb(void){
     usb_manager_t *manager = get_usb_manager();
     manager->bus_status = USB_MOUNTED;
 }
 
 // Invoked when device is unmounted
-void tud_umount_cb(void)
-{
+void tud_umount_cb(void){
     usb_manager_t *manager = get_usb_manager();
     manager->bus_status = USB_NOT_MOUNTED;
 }
@@ -154,16 +141,14 @@ void tud_umount_cb(void)
 // Invoked when usb bus is suspended
 // remote_wakeup_en : if host allow us  to perform remote wakeup
 // Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en)
-{
+void tud_suspend_cb(bool remote_wakeup_en){
     usb_manager_t *manager = get_usb_manager();
     manager->remote_wakeup_en = remote_wakeup_en;
     manager->bus_status = USB_SUSPENDED;
 }
 
 // Invoked when usb bus is resumed
-void tud_resume_cb(void)
-{
+void tud_resume_cb(void){
     usb_manager_t *manager = get_usb_manager();
     manager->bus_status = USB_MOUNTED;
 }
@@ -172,8 +157,7 @@ void tud_resume_cb(void)
 // USB HID
 //--------------------------------------------------------------------+
 
-static void send_hid_report(uint8_t report_id)
-{
+static void send_hid_report(uint8_t report_id){
     // skip if hid is not ready yet
     if ( !tud_hid_ready() ) return;
 
@@ -197,8 +181,7 @@ static void send_hid_report(uint8_t report_id)
 
 // Every 10ms, we will sent 1 report for each HID profile (keyboard, mouse etc ..)
 // tud_hid_report_complete_cb() is used to send the next report after previous one is complete
-void hid_task(void)
-{
+void hid_task(void){
     // Poll every 10ms
     const uint32_t interval_ms = 1;
     static uint32_t start_ms = 0;
@@ -226,16 +209,14 @@ void hid_task(void)
 // Invoked when sent REPORT successfully to host
 // Application can use this to send the next report
 // Note: For composite reports, report[0] is report ID
-void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint8_t len)
-{
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint8_t len){
 
 }
 
 // Invoked when received GET_REPORT control requbuttonest
 // Application must fill buffer report's content and return its length.
 // Return zero will cause the stack to STALL request
-uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
-{
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen){
   // TODO not Implemented
   (void) instance;
   (void) report_id;
@@ -258,8 +239,7 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
 
 // Invoked when received SET_REPORT control request or
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
-void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
-{
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize){
 //   (void) instance;
 //   if (report_id == 2){
 //     // TODO Log what is send by the host Hardware 2.0 is needed.
